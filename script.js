b03206c2e05bd05b31c10df7dee3b859c169a34a
rubenaslanian <aslaniar@uci.edu>
2023-02-04 11:56:04 -0800

    push

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ function artistsearch(artist) {
     console.log(artist.value);
 }
 
+function OnButtonPress() {
+    var artistname = document.getElementById('text_box')
+    artistsearch(artistname);
+    erase(document.getElementById('thecard'));
+}
+
+
 function clickPress(event) {
     if (event.key == "Enter") {
-        artistsearch(document.getElementById('text_box'));
+        var artistname = document.getElementById('text_box')
+        artistsearch(artistname);
+        erase(document.getElementById('thecard'))
     }
+}
+
+function erase(item) {
+    item.remove();
+    create_rest()
 }
