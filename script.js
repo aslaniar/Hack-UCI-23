3e605127640578a1dff4f03901a7f287ac8718af
rubenaslanian <aslaniar@uci.edu>
2023-02-04 19:44:25 -0800

    yay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let liked_songs = {} 
+let liked_songs = []
 let response_list = []
 
 function artistsearch(artist) {
@@ -39,7 +39,13 @@ function callapi(artist) {
         erase(document.getElementById('div1'), document.getElementById('div2'));})
     }
 
-
+function ShiftIt() {
+    document.getElementById('album_art').src = response_list[0][1]
+    document.getElementById('song_title').innerHTML = response_list[0][0]
+    document.getElementById('artist_name').innerHTML = response_list[0][3]
+    document.getElementById('album_name').innerHTML = response_list[0][2]
+    response_list.shift()
+}
 
 function Interested(choice) {
     var album_art = document.getElementById('album_art').value
@@ -47,7 +53,9 @@ function Interested(choice) {
     var artist_name = document.getElementById('artist_name').value
     var album_name = document.getElementById('album_name').value
     if (choice) {
-        liked_songs.append()
+        console.log("Liked.")
+        liked_songs.push([song_name, artist_name, album_art])
+        console.log(liked_songs) // finish this
     }
-
+    ShiftIt()
 }
